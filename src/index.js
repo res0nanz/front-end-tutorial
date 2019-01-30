1a0e21a454377d06865df2ad0c80e26ee39df14c
res0nanz <b1600661@planet.kanazawa-it.ac.jp>
2019-01-30 22:44:22 +0900

    Add ゲームを遡るボタンの実装。ボタンの機能、keyは未実装

--- src/index.js
+++ src/index.js
@@ -96,12 +96,26 @@ class Game extends React.Component {
     const history = this.state.history;
     const current = history[history.length - 1];
     const winner = calculateWinner(current.squares);
+
+    // histroyからstepとmoveに分け、処理
+    const moves = history.map((step, move) => {
+      // moveから文を生成
+      const desc = move ? 'Go to move #' + move : 'Go to game start';
+      return (
+        // ボタン。Arrayからlistをつくる=>keyが必要
+        <li>
+          <button onClick={() => this.jumpTo(move)}>{desc}</button>
+        </li>
+      );
+    });
+
     let status;
     if (winner) {
         status = 'Winner: ' + winner;
     } else {
         status = 'Next player: ' + (this.state.xIsNext ? 'X' : 'O');
     }
+
     return (
       <div className="game">
         <div className="game-board">
@@ -112,7 +126,7 @@ class Game extends React.Component {
         </div>
         <div className="game-info">
           <div>{status}</div>
-          <ol>{/* TODO */}</ol>
+          <ol>{moves}</ol>
         </div>
       </div>
     );
